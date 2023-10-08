6439b9404b81b21d9b409405ff240b1997d07f59
Lucas Ceschini <lucasceschini@ica.ele.puc-rio.br>
2023-10-08 09:05:10 -0300

    initial commit

---
+++ gradcam-synms.py
@@ -0,0 +1,10 @@
+import urllib.request
+import numpy as np
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import clip
+from PIL import Image
+from scipy.ndimage import gaussian_filter
+from torch import nn
+
